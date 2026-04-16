3137265634eb1e7872473679dfa1dc8288ab3fe4
andreuandreu <andreuaprats@gmail.com>
2026-04-16 12:08:40 -0700

    plot distribution change

--- plots/plot_severity_dist.py
+++ plots/plot_severity_dist.py
@@ -12,9 +12,10 @@ def gumbel_pdf(x, mu, beta):
 
 # x range
 x = np.linspace(0, 1, 1000)
-
+x_neg = np.linspace(-1, 0, 1000)
 # Compute absolute value of PDF
 y = abs(gumbel_pdf(x, mu, beta))
+y_neg = abs(gumbel_pdf(x_neg, mu, beta))[::-1]
 y_norm = y / np.sum(y)  # Normalize to sum to 1
 
 severity_sample = np.array([])
@@ -26,8 +27,8 @@ for t in range(622):
 
 # Plot
 plt.figure()
-#plt.plot(x, y_norm*100, label="|Gumbel PDF| (μ=0, β=0.1)")
-#plt.plot(x, y)
+#plt.plot(x, y_norm*100)
+plt.plot(x, y+y_neg, label="|Gumbel PDF| (μ=0, β=0.1)")
 #plt.hist(severity_sample, bins=33, density=True, alpha=0.2, label="Sampled severities", color='orange')
 plt.hist(severity_sample_abs, bins=33, density=True, alpha=0.05, label="Sampled absolute severities", color='blue')
 plt.axvline(x=0.22, color='gray', linestyle='--', label="seb. threshold = 0.22")
